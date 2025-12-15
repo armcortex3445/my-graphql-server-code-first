a314aab293021e4030690c0f4301f70208ec50c6
armcortex3445 <wlghks5966@naver.com>
2025-12-15 21:07:33 +0900

    feat(posts): inject PubSub class as provider

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
+import { PubSub } from 'graphql-subscriptions';
 
 @Module({
-  providers: [PostsResolver, PostsService],
-  exports: [PostsService],
+  providers: [
+    PostsResolver,
+    PostsService,
+    {
+      provide: 'PUB_SUB',
+      useFactory: () => new PubSub(),
+    },
+  ],
+  exports: [PostsService, 'PUB_SUB'],
 })
 export class PostsModule {}
