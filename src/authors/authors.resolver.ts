859b0b7a6bba7b3a5af4a907fa229c389018d578
armcortex3445 <wlghks5966@naver.com>
2025-12-15 17:05:27 +0900

    refactor(authors): refactor code to improve readability

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -1,17 +1,9 @@
-import {
-  Resolver,
-  Query,
-  Mutation,
-  Args,
-  Int,
-  ResolveField,
-  Parent,
-} from '@nestjs/graphql';
+import { Resolver, Query, Args, ResolveField, Parent } from '@nestjs/graphql';
 import { AuthorsService } from './authors.service';
 import { Author } from './models/author.model';
-import { CreateAuthorInput } from './dto/create-author.input';
-import { UpdateAuthorInput } from './dto/update-author.input';
 import { PostsService } from '../posts/posts.service';
+import { Post } from '../posts/models/post.model';
+import { GetAuthorArgs } from './dto/get-author.args';
 
 @Resolver(() => Author)
 export class AuthorsResolver {
@@ -20,13 +12,14 @@ export class AuthorsResolver {
     private readonly postsService: PostsService,
   ) {}
 
-  @Query(() => Author)
-  author(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Author, { name: 'author' })
+  getAuthor(@Args() args: GetAuthorArgs) {
+    const { id } = args;
     return this.authorsService.findOne(id);
   }
 
-  @ResolveField()
-  posts(@Parent() author: Author) {
+  @ResolveField('posts', () => [Post])
+  getPosts(@Parent() author: Author) {
     const { id } = author;
     return this.postsService.findAll({ authorId: id });
   }
