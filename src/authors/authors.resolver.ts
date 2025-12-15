d3448185cdb8b9fc1ee9a8117859c8b96e202177
armcortex3445 <wlghks5966@naver.com>
2025-12-15 21:08:08 +0900

    feat(authors): add commentAdded Subscription

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -1,15 +1,26 @@
-import { Resolver, Query, Args, ResolveField, Parent } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  ResolveField,
+  Parent,
+  Subscription,
+} from '@nestjs/graphql';
 import { AuthorsService } from './authors.service';
 import { Author } from './models/author.model';
 import { PostsService } from '../posts/posts.service';
 import { Post } from '../posts/models/post.model';
 import { GetAuthorArgs } from './dto/get-author.args';
+import { PubSub } from 'graphql-subscriptions';
+import { Comment } from '../posts/models/comment.model';
+import { Inject } from '@nestjs/common';
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   constructor(
     private readonly authorsService: AuthorsService,
     private readonly postsService: PostsService,
+    @Inject('PUB_SUB') private readonly pubSub: PubSub,
   ) {}
 
   @Query(() => Author, { name: 'author' })
@@ -22,5 +33,10 @@ export class AuthorsResolver {
   getPosts(@Parent() author: Author) {
     const { id } = author;
     return this.postsService.findAll({ authorId: id });
+  }
+
+  @Subscription(() => Comment, { name: 'commentAdded' })
+  subscribeToCommentAdded() {
+    return this.pubSub.asyncIterableIterator('commentAdded');
   }
 }
